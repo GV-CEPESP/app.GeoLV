25bcd34793eeffc820ebd26b6489a52f7dde2eb9
Rafael Coelho <rafael.coelho.x@gmail.com>
2018-04-20 12:23:45 -0400

    update

--- app.R
+++ app.R
@@ -2,32 +2,29 @@ library(shiny)
 library(leaflet)
 library(magrittr)
 library(dplyr)
+library(shinythemes)
 
 
 # Define UI for application that draws a histogram
-ui <- bootstrapPage(
-  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leafletOutput("map",  width = "100%", height = "100%"),
-  absolutePanel(top = 10, right = 10,
-                sliderInput("n", min = 1, max= 1008, value = c(1, 1008),
-                            label = "Faixa de Observações"),
-                checkboxGroupInput("provider",
-                                   label = "Qual provedor?",
-                                   choiceNames = c("ArcGIS", "Google Maps", "Here"),
-                                   choiceValues = c("arcgis_online", "google_maps", "here_geocoder"),
-                                   selected = c("arcgis_online", "google_maps", "here_geocoder")))
-                 )
+ui <- bootstrapPage("GeoLV",
+                 tabPanel("Teste",tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+                                       leafletOutput("map",  width = "100%", height = "100%"),
+                          bootstrapPage(absolutePanel(top = 70, right = 10,
+                                                      sliderInput("n", min = 1, max= 1040, value = c(1, 1040),
+                                                                  label = "Faixa de Observações"),
+                                                      checkboxGroupInput("provider", 
+                                                                         label = "Qual provedor?",
+                                                                         choiceNames = c("ArcGIS", "Google Maps", "Here"),
+                                                                         choiceValues = c("arcgis_online", "google_maps", "here_geocoder"),
+                                                                         selected = c("arcgis_online", "google_maps", "here_geocoder"))))))
    
-
-
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
   geolv <- reactive({
     readr::read_rds("banco.rds") %>% 
-      slice(input$n[1]:input$n[2]) %>% 
-      filter(provider %in% input$provider)
+      slice(input$n[1]:input$n[2])
     })
   
   icons <- reactive({
